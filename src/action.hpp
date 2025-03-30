c7f28568691d372a0bb8e6e916aa20af47929e6f
Riliumph <riliumph@outlook.jp>
2025-03-30 18:16:26 +0000

    [add] 代表ヘッダを追加

---
+++ src/action.hpp
@@ -0,0 +1,8 @@
+#ifndef ACTION_HPP
+#define ACTION_HPP
+#include "action/action_if.hpp"
+#include "action/fly.hpp"
+#include "action/load.hpp"
+#include "action/load_side.hpp"
+#include "action/move.hpp"
+#endif // ACTION_HPP
---
+++ src/machine.hpp
@@ -0,0 +1,7 @@
+#ifndef MACHINE_HPP
+#define MACHINE_HPP
+#include "machine/drone.hpp"
+#include "machine/forklift_3way.hpp"
+#include "machine/forklift_reach.hpp"
+#include "machine/machine_if.hpp"
+#endif // MACHINE_HPP
